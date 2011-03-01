60a32de3c9e822896d78b0a427ac89b2e310f08c
amilamanoj@gmail.com <amilamanoj@gmail.com@47f8494c-5e97-89ce-185d-fe7e084c7765>
2011-03-01 11:16:16 +0000

    model rename

---
+++ trunk/HRSystemOopNB/src/hrsystemoop/model/HREmployer.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hrsystemoop.model;
+
+/**
+ *
+ * @author prasath
+ */
+public interface HREmployer extends Employee {
+
+    
+}
