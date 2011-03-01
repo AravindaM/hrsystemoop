55405ab8d9f2ba337ebc906135378fd5d3dff236
rcmperera@gmail.com <rcmperera@gmail.com@47f8494c-5e97-89ce-185d-fe7e084c7765>
2011-03-01 06:27:24 +0000

    first revision , essentials , part 1

---
+++ trunk/HRSystemOopNB/src/hrsystemoop/modle/Employee.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hrsystemoop.modle;
+
+/**
+ *
+ * @author R.C.M. Perera
+ */
+public interface Employee {
+
+    int getId();
+
+    String getName();
+
+    void setName();
+
+}
