e909beffd5faf914ef1baaf22207b9a504ba390c
rcmperera@gmail.com <rcmperera@gmail.com@47f8494c-5e97-89ce-185d-fe7e084c7765>
2011-03-02 10:12:14 +0000

    javadoc

--- trunk2/HRSystemOopNB/src/hrsystemoop/actions/UserCommands.java
+++ trunk2/HRSystemOopNB/src/hrsystemoop/actions/UserCommands.java
@@ -1,16 +1,21 @@
 package hrsystemoop.actions;
 
-import java.util.Collections;
 import java.util.*;
 
 /**
  *
+ * this is immutable set of command.
+ * each Employee should have UserCommands Object
  * @author R.C.M. Perera
  */
 public class UserCommands {
 
     private Map<String, Command> commands;
 
+    /**
+     * construct a UserCommands all the Command in the give array
+     * @param commandsArr array of Command objects to be included
+     */
     public UserCommands(Command[] commandsArr) {
         commands = new TreeMap<String, Command>();
         for (Command command : commandsArr) {
@@ -18,10 +23,20 @@ public class UserCommands {
         }
     }
 
+    /**
+     * names of all the available commands
+     * @return unmodifiable set of names
+     */
     public Set<String> getAvailabeCommands() {
         return Collections.unmodifiableSet(commands.keySet());
     }
 
+    /**
+     * select a command by name
+     * @param commandName name of the requested Command.
+     * should be as same as <code>Command.getName(String)</code> value of the Command.
+     * @return Command with the given name
+     */
     public Command getCommand(String commandName) {
         return commands.get(commandName);
     }
