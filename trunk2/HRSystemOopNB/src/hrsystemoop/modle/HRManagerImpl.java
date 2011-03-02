99ba437f30675b944fde27214deb3f61ab792b73
n.prasath.002@gmail.com <n.prasath.002@gmail.com@47f8494c-5e97-89ce-185d-fe7e084c7765>
2011-03-02 07:24:13 +0000

    More commands added for HR employee

--- trunk2/HRSystemOopNB/src/hrsystemoop/modle/HRManagerImpl.java
+++ trunk2/HRSystemOopNB/src/hrsystemoop/modle/HRManagerImpl.java
@@ -5,11 +5,20 @@
 
 package hrsystemoop.modle;
 
+import hrsystemoop.actions.AcceptLoanCommand;
 import hrsystemoop.actions.AddEmployeeCommand;
+import hrsystemoop.actions.CheckSalaryCommand;
 import hrsystemoop.actions.Command;
+import hrsystemoop.actions.CommandContext;
+import hrsystemoop.actions.RemoveEmployeeCommand;
+import hrsystemoop.actions.RequestLeaveCommand;
+import hrsystemoop.actions.RequestLoanCommand;
 
 import hrsystemoop.actions.UserCommands;
+import hrsystemoop.actions.show.ShowSelfAttendanceSummaryCommand;
+import hrsystemoop.actions.show.ShowSelfCommand;
 import hrsystemoop.actions.show.ShowSelfIDCommand;
+import hrsystemoop.actions.show.ShowSelfNameCommand;
 
 /**
  *
@@ -19,8 +28,15 @@ public class HRManagerImpl extends HREmployerImpl implements HRManager {
 
     static final UserCommands commands = new UserCommands(new Command[]{
         new ShowSelfIDCommand(),
-        new AddEmployeeCommand()
-        //acceptleave command, add remove update employee employeee
+        new AddEmployeeCommand(),
+        new AcceptLoanCommand(),
+        new CheckSalaryCommand(),
+        new RemoveEmployeeCommand(),
+        new RequestLeaveCommand(),
+        new RequestLoanCommand(),
+        new ShowSelfAttendanceSummaryCommand(),
+        new ShowSelfIDCommand(),
+        new ShowSelfNameCommand()
 
     });
 
