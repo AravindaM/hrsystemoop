15867b26debb8e32e843a0c39ea2afeb3ca1e255
rcmperera@gmail.com <rcmperera@gmail.com@47f8494c-5e97-89ce-185d-fe7e084c7765>
2011-03-02 03:31:56 +0000

    javaDoc update

--- trunk2/HRSystemOopNB/src/hrsystemoop/database/Database.java
+++ trunk2/HRSystemOopNB/src/hrsystemoop/database/Database.java
@@ -39,15 +39,32 @@ public abstract class Database {
      */
     public abstract Employee getEmployee(String name) throws DatabaseExeption;
 
+    /**
+     * the Employee that is currently has the <code>id</code> will be replaced by <code>emp</code>
+     * <br/> the newly updated Employee's id will also be updated to <code>emp.getID()</code>
+     * @param id id of the Employee to be updated
+     * @param emp Employee object that will replace the
+     * @throws DatabaseExeption thrown if no Employee exist by given id
+     */
     public abstract void updateEmployee(int id, Employee emp) throws DatabaseExeption;
+
+
 
     /**
      * adds a new Employee to the database.
      * but does not save(commit) it to actual database
-     *
+     * <br/> new Employee's id will set to the Employee through <code>Employee.setID()</code>
      * @param emp Employee to be added
+     * @return newly assigned id of the Employee
+     * @throws DatabaseExeption thrown if there is conflicting user names
      */
     public abstract int addEmployee(Employee emp) throws DatabaseExeption;
 
+    /**
+     * delete the Employee with the given id
+     * @param id id of the Employee to be deleted
+     * @return deleted Employee
+     * @throws DatabaseExeption thrown if no Employee exist by given id
+     */
     public abstract Employee deleteEmployee(int id) throws DatabaseExeption;
 }
